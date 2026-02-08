d0c25da43878d75cc9585fe463faa7bf572707ab
TadeoAhat <taha.tadeo@gmail.com>
2026-02-08 21:26:57 +0100

    Add files via upload

---
+++ version.py
@@ -0,0 +1,21 @@
+import json
+
+with open("example.cimg", "rb") as file: #rb je binarny mod na citanie a with zabezpeci ze sa subor VZDY zavrie po skonceni bloku
+    magic = file.read(4) #citam prve styri bajty zo suboru
+    assert magic == b"CIMG" #kontrolujeme ci su presne to co chceme, inak sa stane raise AssertionError
+    meta_len = int.from_bytes(file.read(4), "big") #dalsie 4 bajty, ocakava sa 32bitove cislo, big znamena big-endian poradie bajtov (najvyznamnejsi 1.)
+    meta = json.loads(file.read(meta_len).decode()) # json.loads zmeni na zoznam .decode prevedie bajty na str. 
+    rest = file.read()
+    print("metadata:", meta)
+    print("data snippet:", rest[:200])
+
+
+    #DOPLNAM KOD Z MINULEJ ULOHY
+    if isinstance(meta, dict): #iba checknem ci je to slovnik, ak hej dostanem info
+        version = meta.get("VersionInformation")    
+        version_date = meta.get("VersionDate")     
+        version_note = meta.get("VersionNote")      
+    else:
+        version = None
+        version_date = None
+        version_note = None
